0e918643cb2f91022466e7796edaada83eb9daa5
Jan Walter <jnwltr@gmail.com>
2017-10-24 00:41:08 +0200

    1.0.10

--- dist/utils.js
+++ dist/utils.js
@@ -66,7 +66,7 @@ exports.indent = indent;
  * @param content
  */
 function writeFile(file, content, header) {
-    const disable = '/* tslint:disable:max-line-lengtx */';
+    const disable = '/* tslint:disable:max-line-length */';
     content = `${disable}\n${header}\n${content}`;
     fs.writeFileSync(file, content);
     out(`${file} generated`, 'green');
